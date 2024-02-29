505e49f1cf442096a8a6746a3e9f1e021acff5e2
gentur widyaputra <gentur.wp@gmail.com>
2024-02-29 21:25:47 +0700

    fix bug in mapping subsatuankerjaid into dsp file

--- eagle/__init__.py
+++ eagle/__init__.py
@@ -7,6 +7,58 @@ from eagle.models.raw_dsp import RawDsp
 from werkzeug.utils import secure_filename
 from eagle.models.raw_comparison import RawComparison
 import pandas as pd
+import numpy as np
+
+
+def read_dsp_file(dspfile) -> pd.DataFrame:
+    df = pd.read_excel(dspfile, usecols="A:M", header=None)
+    df = df.rename(
+        columns={
+            0: "dsp_nomor",
+            1: "dsp_jabatan",
+            2: "dsp_gol_jab",
+            3: "dsp_pangkat",
+            4: "dsp_korps",
+            5: "dsp_bidang_profesi",
+            6: "dsp_spesialisasi",
+            7: "dsp_pa",
+            8: "dsp_ba",
+            9: "dsp_ta",
+            10: "dsp_pns",
+            11: "dsp_jml",
+            12: "dsp_ket",
+        }
+    )
+    df = df.dropna(how="all", subset=["dsp_jabatan"])
+    df = df.replace(r" ", np.nan)
+    df["dsp_jabatan"] = df["dsp_jabatan"].str.strip().str.lower()
+    df["dsp_pangkat"] = df["dsp_pangkat"].replace("\n", " ", regex=True)
+    df["dsp_korps"] = df["dsp_korps"].replace("\n", " ", regex=True)
+    df["dsp_bidang_profesi"] = df["dsp_bidang_profesi"].replace("\n", " ", regex=True)
+    df["dsp_spesialisasi"] = df["dsp_spesialisasi"].replace("\n", " ", regex=True)
+    df["dsp_ket"] = df["dsp_ket"].replace("\n", " ", regex=True)
+    df = df.query("dsp_jabatan != '`'")
+    df = df.query("dsp_jabatan != '2'")
+    df = df.query("dsp_jabatan != 'J A B A T A N'")
+    df = df.query("dsp_jabatan != 'JUMLAH'")
+    df = df.query("dsp_jabatan != 'FUNGSIONAL'")
+    df = df.query("dsp_pa != 'P A N G K A T'")
+    df = df.query("dsp_pa != 'P A N G K A T '")
+    df = df.query("dsp_pa != 'PANGKAT'")
+    df = df.query("dsp_pa != '         PANGKAT'")
+    df = df.query("dsp_ta != '          '")
+    df = df.query("dsp_ba != 'P A N G K A T'")
+    df = df.query("dsp_ba != 'P A N G K A T '")
+    df.astype(
+        {
+            "dsp_pa": float,
+            "dsp_ba": float,
+            "dsp_ta": float,
+            "dsp_pns": float,
+            "dsp_jml": float,
+        }
+    )
+    return df
 
 
 def create_app(config_class=Config):
@@ -30,9 +82,9 @@ def create_app(config_class=Config):
         if not filename:
             resp = {"status": 400, "message": "no selected file"}
             return jsonify(resp), 400
-        if filename.lower().rsplit(".", 1)[1] != "xls":
-            resp = {"status": 400, "message": "only accept xls file"}
-            return jsonify(resp), 400
+        # if filename.lower().rsplit(".", 1)[1] != "xls":
+        #    resp = {"status": 400, "message": "only accept xls file"}
+        #    return jsonify(resp), 400
 
         form_data = request.form.to_dict()
         if form_data.get("satuankerja_id") is None or not (
@@ -54,8 +106,13 @@ def create_app(config_class=Config):
             }
             return jsonify(resp), 400
 
-        delete_stmt = delete(RawDsp).where(
-            RawDsp.dsp_nomor_keputusan_kasau == form_data.get("nomor_keputusan_kasau")
+        delete_stmt = (
+            delete(RawDsp)
+            .where(
+                RawDsp.dsp_nomor_keputusan_kasau
+                == form_data.get("nomor_keputusan_kasau")
+            )
+            .where(RawDsp.dsp_satuankerja_id == form_data.get("satuankerja_id"))
         )
         db.session.execute(delete_stmt)
         db.session.commit()
@@ -65,63 +122,10 @@ def create_app(config_class=Config):
         )
 
         dsp_file_path = app.config["UPLOAD_FOLDER"] + secure_filename(filename)
-        df = pd.read_excel(dsp_file_path, dtype=str)
-        df.rename(
-            columns={
-                "Unnamed: 0": "dsp_nomor",
-                "Unnamed: 1": "dsp_jabatan",
-                "Unnamed: 2": "dsp_gol_jab",
-                "Unnamed: 3": "dsp_pangkat",
-                "Unnamed: 4": "dsp_korps",
-                "Unnamed: 5": "dsp_bidang_profesi",
-                "Unnamed: 6": "dsp_spesialisasi",
-                "Unnamed: 7": "dsp_pa",
-                "Unnamed: 8": "dsp_ba",
-                "Unnamed: 9": "dsp_ta",
-                "Unnamed: 10": "dsp_pns",
-                "Unnamed: 11": "dsp_jml",
-                "Unnamed: 12": "dsp_ket",
-            },
-            inplace=True,
-        )
-        df["dsp_pa"] = df["dsp_pa"].str.strip()
-        df.dropna(how="all", subset=["dsp_jabatan"], inplace=True)
-        df.query(
-            "dsp_jabatan != 'J A B A T A N' and dsp_jabatan != 'JUMLAH' and dsp_jabatan != '2'",
-            inplace=True,
-        )
-        df["dsp_jabatan"] = df["dsp_jabatan"].str.strip().str.lower()
-        df["dsp_pangkat"] = df["dsp_pangkat"].replace("\n", " ", regex=True)
-        df["dsp_korps"] = df["dsp_korps"].replace("\n", " ", regex=True)
-        df["dsp_bidang_profesi"] = df["dsp_bidang_profesi"].replace(
-            "\n", " ", regex=True
-        )
-        df["dsp_spesialisasi"] = df["dsp_spesialisasi"].replace("\n", " ", regex=True)
-        df["dsp_ket"] = df["dsp_ket"].replace("\n", " ", regex=True)
+        df = read_dsp_file(dsp_file_path)
         df["dsp_satuankerja_id"] = form_data.get("satuankerja_id")
         df["dsp_satuankerja_nama"] = form_data.get("satuankerja_nama")
         df["dsp_nomor_keputusan_kasau"] = form_data.get("nomor_keputusan_kasau")
-        df["dsp_nomor"] = df["dsp_nomor"].replace(" ", None)
-        df["dsp_pa"] = df["dsp_pa"].replace(r"\D+", None)
-        df["dsp_ba"] = df["dsp_ba"].replace(r"\D+", None)
-        df["dsp_ta"] = df["dsp_ta"].replace(r"\D+", None)
-        df["dsp_pns"] = df["dsp_pns"].replace(r"\D+", None)
-        df["dsp_pa"] = df["dsp_pa"].replace("", None)
-        df["dsp_ba"] = df["dsp_ba"].replace("", None)
-        df["dsp_ta"] = df["dsp_ta"].replace("", None)
-        df["dsp_pns"] = df["dsp_pns"].replace("", None)
-        df["dsp_pa"] = df["dsp_pa"].replace(" ", None)
-        df["dsp_ba"] = df["dsp_ba"].replace(" ", None)
-        df["dsp_ta"] = df["dsp_ta"].replace(" ", None)
-        df["dsp_pns"] = df["dsp_pns"].replace(" ", None)
-        df.astype(
-            {
-                "dsp_pa": "float",
-                "dsp_ba": "float",
-                "dsp_ta": "float",
-                "dsp_pns": "float",
-            }
-        )
 
         df.to_sql(
             name="raw_dsp",
@@ -146,6 +150,7 @@ def create_app(config_class=Config):
         for jbt in raw_dsp_data.scalars():
             jabatan_nama_list.append(jbt.dsp_jabatan)
         jabatan_nama_concat = "'" + "','".join(jabatan_nama_list) + "'"
+        print(f"jabatan_nama_concat = {jabatan_nama_concat}")
         jabatan_subsatuankerja = db.session.execute(
             text(
                 f"""
@@ -175,19 +180,19 @@ def create_app(config_class=Config):
         jabatan_satuankerja = db.session.execute(
             text(
                 f"""
-                select                                                          
-                    a.struktur_id,                                              
-                    trim(lower(a.jabatan_nama)) as jabatan_nama,                
+                select
+                    a.struktur_id,
+                    trim(lower(a.jabatan_nama)) as jabatan_nama,
                     trim(lower(a.jabatan_nama_panjang)) as jabatan_nama_panjang,
-                    a.jumlah_perwira,                                           
-                    a.jumlah_bintara,                                           
-                    a.jumlah_tamtama,                                           
-                    a.jumlah_pns,                                               
-                    a.jumlah_total                                              
-                from raw.raw_m_jabatan  a                                       
-                where a.jabatan_status=1                                        
-                and trim(lower(a.jabatan_nama)) in ({jabatan_nama_concat})           
-                and a.struktur_id = '{form_data.get("satuankerja_id")}'                  
+                    a.jumlah_perwira,
+                    a.jumlah_bintara,
+                    a.jumlah_tamtama,
+                    a.jumlah_pns,
+                    a.jumlah_total
+                from raw.raw_m_jabatan  a
+                where a.jabatan_status=1
+                and trim(lower(a.jabatan_nama)) in ({jabatan_nama_concat})
+                and a.struktur_id = '{form_data.get("satuankerja_id")}'
                 """
             )
         )
@@ -283,9 +288,15 @@ def create_app(config_class=Config):
             counter = counter + 1
 
         try:
-            delete_stmt = delete(RawComparison).where(
-                RawComparison.dsp_nomor_keputusan_kasau
-                == form_data.get("nomor_keputusan_kasau")
+            delete_stmt = (
+                delete(RawComparison)
+                .where(
+                    RawComparison.dsp_nomor_keputusan_kasau
+                    == form_data.get("nomor_keputusan_kasau")
+                )
+                .where(
+                    RawComparison.dsp_satuankerja_id == form_data.get("satuankerja_id")
+                )
             )
             db.session.execute(delete_stmt)
             db.session.execute(insert(RawComparison), dsp_jabatan_list)
