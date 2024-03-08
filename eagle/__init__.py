c3af1de079dae7d76dd4f5fbbe666f22199a5b70
gentur widyaputra <gentur.wp@gmail.com>
2024-03-08 16:20:03 +0700

    generate dsp hierarcy before lookup to subsatuankerja

--- eagle/__init__.py
+++ eagle/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, request, Response
 from sqlalchemy import select, delete, text, asc, insert
 from config import Config
 from eagle.extensions import db
@@ -32,6 +32,7 @@ def read_dsp_file(dspfile) -> pd.DataFrame:
     df = df.dropna(how="all", subset=["dsp_jabatan"])
     df = df.replace(r" ", np.nan)
     df["dsp_jabatan"] = df["dsp_jabatan"].str.strip().str.lower()
+    df["dsp_jabatan"] = df["dsp_jabatan"].replace("\n", " ", regex=True)
     df["dsp_pangkat"] = df["dsp_pangkat"].replace("\n", " ", regex=True)
     df["dsp_korps"] = df["dsp_korps"].replace("\n", " ", regex=True)
     df["dsp_bidang_profesi"] = df["dsp_bidang_profesi"].replace("\n", " ", regex=True)
@@ -42,6 +43,7 @@ def read_dsp_file(dspfile) -> pd.DataFrame:
     df = df.query("dsp_jabatan != 'J A B A T A N'")
     df = df.query("dsp_jabatan != 'JUMLAH'")
     df = df.query("dsp_jabatan != 'FUNGSIONAL'")
+    df = df.query("dsp_jabatan != 'jumlah'")
     df = df.query("dsp_pa != 'P A N G K A T'")
     df = df.query("dsp_pa != 'P A N G K A T '")
     df = df.query("dsp_pa != 'PANGKAT'")
@@ -76,27 +78,24 @@ def create_app(config_class=Config):
     def upload_dsp():
         if "dsp_file" not in request.files:
             resp = {"status": 400, "message": "dsp_file should not be empty"}
-            return jsonify(resp), 400
+            return resp, 400
         dsp_file = request.files["dsp_file"]
         filename = dsp_file.filename
         if not filename:
             resp = {"status": 400, "message": "no selected file"}
-            return jsonify(resp), 400
-        # if filename.lower().rsplit(".", 1)[1] != "xls":
-        #    resp = {"status": 400, "message": "only accept xls file"}
-        #    return jsonify(resp), 400
+            return resp, 400
 
         form_data = request.form.to_dict()
         if form_data.get("satuankerja_id") is None or not (
             form_data.get("satuankerja_id")
         ):
             resp = {"status": 400, "message": "satuankerja_id should not be empty"}
-            return jsonify(resp), 400
+            return resp, 400
         if form_data.get("satuankerja_nama") is None or not (
             form_data.get("satuankerja_nama")
         ):
             resp = {"status": 400, "message": "satuankerja_nama should not be empty"}
-            return jsonify(resp), 400
+            return resp, 400
         if form_data.get("nomor_keputusan_kasau") is None or not (
             form_data.get("nomor_keputusan_kasau")
         ):
@@ -104,7 +103,20 @@ def create_app(config_class=Config):
                 "status": 400,
                 "message": "nmmor_keputusan_kasau should not be empty",
             }
-            return jsonify(resp), 400
+            return resp, 400
+
+        check_satuankerja_stmt = db.session.execute(
+            text(
+                f"select count(1) from raw.raw_s_satuankerja where satuankerja_id ='{form_data.get('satuankerja_id')}'"
+            )
+        )
+        satuankerja_exists = check_satuankerja_stmt.one()
+        if satuankerja_exists[0] != 1:
+            resp = {
+                "status": 404,
+                "message": "satuankerja_id does not exists",
+            }
+            return resp, 404
 
         delete_stmt = (
             delete(RawDsp)
@@ -141,6 +153,7 @@ def create_app(config_class=Config):
                 == form_data.get("nomor_keputusan_kasau")
             )
             .where(RawDsp.dsp_satuankerja_id == form_data.get("satuankerja_id"))
+            .where(RawDsp.dsp_jabatan != None)
             .order_by(asc(RawDsp.id))
         )
         raw_dsp_data = db.session.execute(raw_dsp_stmt)
@@ -150,7 +163,6 @@ def create_app(config_class=Config):
         for jbt in raw_dsp_data.scalars():
             jabatan_nama_list.append(jbt.dsp_jabatan)
         jabatan_nama_concat = "'" + "','".join(jabatan_nama_list) + "'"
-        print(f"jabatan_nama_concat = {jabatan_nama_concat}")
         jabatan_subsatuankerja = db.session.execute(
             text(
                 f"""
@@ -176,7 +188,6 @@ def create_app(config_class=Config):
             )
         )
         jab_subsat = jabatan_subsatuankerja.fetchall()
-
         jabatan_satuankerja = db.session.execute(
             text(
                 f"""
@@ -191,7 +202,7 @@ def create_app(config_class=Config):
                     a.jumlah_total
                 from raw.raw_m_jabatan  a
                 where a.jabatan_status=1
-                and trim(lower(a.jabatan_nama)) in ({jabatan_nama_concat})
+                and trim(lower(a.jabatan_nama)) in ({jabatan_nama_concat.lower()})
                 and a.struktur_id = '{form_data.get("satuankerja_id")}'
                 """
             )
@@ -207,16 +218,38 @@ def create_app(config_class=Config):
                 del dsp_jabatan["updated_at"]
             if "id" in dsp_jabatan.keys():
                 del dsp_jabatan["id"]
-            jab_exists = False
+            subsatker_exists = dsp_jabatan["dsp_jabatan"] in [
+                subsat[1] for subsat in jab_subsat
+            ]
+            parent = None
+            ####cari parent node
             if dsp_jabatan["dsp_nomor"] is not None:
-                for jab in jab_subsat:
-                    if jab[1] == dsp_jabatan["dsp_jabatan"]:
-                        jab_exists = True
-                        parent_id = jab[8]
-                        if jab[8] == "":
-                            parent_node = dsp_jabatan_list[0]
-                            parent_id = parent_node["sisfopers_struktur_id"]
+                nomor_node = dsp_jabatan["dsp_nomor"].rstrip(".")
+                is_child_node = "." in nomor_node
+                if not is_child_node:
+                    if len(dsp_jabatan_list) != 0:
+                        parent = dsp_jabatan_list[0]
+                else:
+                    parent_nomor = nomor_node[: nomor_node.rindex(".")]
+                    start = len(dsp_jabatan_list) - 1
+                    for idx in range(start, -1, -1):
+                        if dsp_jabatan_list[idx]["dsp_nomor"] is not None:
+                            dsp_parent = dsp_jabatan_list[idx]["dsp_nomor"].rstrip(".")
+                            if parent_nomor == dsp_parent:
+                                parent = dsp_jabatan_list[idx]
+            else:
+                start = len(dsp_jabatan_list) - 1
+                for idx in range(start, -1, -1):
+                    if dsp_jabatan_list[idx]["dsp_nomor"] is not None:
+                        parent = dsp_jabatan_list[idx]
+                        break
 
+            if parent is None:
+                exists_in_jabatan = dsp_jabatan["dsp_jabatan"] in [
+                    jab[1] for jab in jab_sat if jab[1] == dsp_jabatan["dsp_jabatan"]
+                ]
+                if exists_in_jabatan:
+                    for jab in jab_sat:
                         dsp_jabatan["sisfopers_struktur_id"] = jab[0]
                         dsp_jabatan["sisfopers_jabatan_nama_panjang"] = jab[2]
                         dsp_jabatan["sisfopers_jumlah_perwira"] = jab[3]
@@ -224,65 +257,72 @@ def create_app(config_class=Config):
                         dsp_jabatan["sisfopers_jumlah_tamtama"] = jab[5]
                         dsp_jabatan["sisfopers_jumlah_pns"] = jab[6]
                         dsp_jabatan["sisfopers_jumlah_total"] = jab[7]
-                        dsp_jabatan["sisfopers_parent_id"] = parent_id
-                        dsp_jabatan["sisfopers_subsatuankerja_id"] = jab[9]
+                        dsp_jabatan["sisfopers_parent_id"] = ""
+                        dsp_jabatan["sisfopers_subsatuankerja_id"] = ""
                         dsp_jabatan["compare_status"] = 1
-                        break
+                else:
+                    dsp_jabatan["sisfopers_struktur_id"] = ""
+                    dsp_jabatan["sisfopers_jabatan_nama_panjang"] = ""
+                    dsp_jabatan["sisfopers_jumlah_perwira"] = None
+                    dsp_jabatan["sisfopers_jumlah_bintara"] = None
+                    dsp_jabatan["sisfopers_jumlah_tamtama"] = None
+                    dsp_jabatan["sisfopers_jumlah_pns"] = None
+                    dsp_jabatan["sisfopers_jumlah_total"] = None
+                    dsp_jabatan["sisfopers_parent_id"] = ""
+                    dsp_jabatan["sisfopers_subsatuankerja_id"] = ""
+                    dsp_jabatan["compare_status"] = 0
+
             else:
-                for idx in range(len(dsp_jabatan_list) - 1, -1, -1):
-                    parent = dsp_jabatan_list[idx]
-                    if parent["dsp_nomor"] is not None:
-                        if "sisfopers_subsatuankerja_id" in parent.keys():
+                if subsatker_exists:
+                    dsp_subsat = [
+                        dsp_insubsat
+                        for dsp_insubsat in jab_subsat
+                        if dsp_insubsat[1] == dsp_jabatan["dsp_jabatan"]
+                    ]
+                    for subsat in dsp_subsat:
+                        if (
+                            dsp_jabatan["dsp_jabatan"] == subsat[1]
+                            and subsat[8] == parent["sisfopers_subsatuankerja_id"]
+                        ):
+                            dsp_jabatan["sisfopers_struktur_id"] = subsat[0]
+                            dsp_jabatan["sisfopers_jabatan_nama_panjang"] = subsat[2]
+                            dsp_jabatan["sisfopers_jumlah_perwira"] = subsat[3]
+                            dsp_jabatan["sisfopers_jumlah_bintara"] = subsat[4]
+                            dsp_jabatan["sisfopers_jumlah_tamtama"] = subsat[5]
+                            dsp_jabatan["sisfopers_jumlah_pns"] = subsat[6]
+                            dsp_jabatan["sisfopers_jumlah_total"] = subsat[7]
                             dsp_jabatan["sisfopers_parent_id"] = parent[
-                                "sisfopers_struktur_id"
+                                "sisfopers_subsatuankerja_id"
                             ]
+                            dsp_jabatan["sisfopers_subsatuankerja_id"] = subsat[9]
                             dsp_jabatan["sisfopers_parent_nomor"] = parent["dsp_nomor"]
-                            dsp_jabatan["sisfopers_jumlah_perwira"] = 0
-                            dsp_jabatan["sisfopers_jumlah_bintara"] = 0
-                            dsp_jabatan["sisfopers_jumlah_tamtama"] = 0
-                            dsp_jabatan["sisfopers_jumlah_pns"] = 0
-                            dsp_jabatan["sisfopers_jumlah_total"] = 0
-                            dsp_jabatan["compare_status"] = 0
-                            for jab in jab_subsat:
-                                if (
-                                    jab[1] == dsp_jabatan["dsp_jabatan"]
-                                    and jab[8] == dsp_jabatan["sisfopers_parent_id"]
-                                ):
-                                    dsp_jabatan["sisfopers_struktur_id"] = jab[0]
-                                    dsp_jabatan["sisfopers_jabatan_nama_panjang"] = jab[
-                                        2
-                                    ]
-                                    dsp_jabatan["sisfopers_jumlah_perwira"] = jab[3]
-                                    dsp_jabatan["sisfopers_jumlah_bintara"] = jab[4]
-                                    dsp_jabatan["sisfopers_jumlah_tamtama"] = jab[5]
-                                    dsp_jabatan["sisfopers_jumlah_pns"] = jab[6]
-                                    dsp_jabatan["sisfopers_jumlah_total"] = jab[7]
-                                    dsp_jabatan["sisfopers_subsatuankerja_id"] = jab[9]
-                                    dsp_jabatan["compare_status"] = 1
-                                    break
+                            dsp_jabatan["compare_status"] = 1
+                            break
                         else:
-                            dsp_jabatan["sisfopers_parent_nomor"] = parent["dsp_nomor"]
-                            dsp_jabatan["compare_status"] = 0
-                            dsp_jabatan["sisfopers_jumlah_perwira"] = 0
-                            dsp_jabatan["sisfopers_jumlah_bintara"] = 0
-                            dsp_jabatan["sisfopers_jumlah_tamtama"] = 0
-                            dsp_jabatan["sisfopers_jumlah_pns"] = 0
-                            dsp_jabatan["sisfopers_jumlah_total"] = 0
-                        break
-            if not jab_exists:
-                for jab in jab_sat:
-                    if jab[1] == dsp_jabatan["dsp_jabatan"]:
-                        if dsp_jabatan["dsp_jabatan"] is not None:
-                            dsp_jabatan["sisfopers_struktur_id"] = jab[0]
-                            dsp_jabatan["sisfopers_jabatan_nama_panjang"] = jab[2]
-                            dsp_jabatan["sisfopers_jumlah_perwira"] = jab[3]
-                            dsp_jabatan["sisfopers_jumlah_bintara"] = jab[4]
-                            dsp_jabatan["sisfopers_jumlah_tamtama"] = jab[5]
-                            dsp_jabatan["sisfopers_jumlah_pns"] = jab[6]
-                            dsp_jabatan["sisfopers_jumlah_total"] = jab[7]
+                            dsp_jabatan["sisfopers_struktur_id"] = ""
+                            dsp_jabatan["sisfopers_jabatan_nama_panjang"] = ""
+                            dsp_jabatan["sisfopers_jumlah_perwira"] = None
+                            dsp_jabatan["sisfopers_jumlah_bintara"] = None
+                            dsp_jabatan["sisfopers_jumlah_tamtama"] = None
+                            dsp_jabatan["sisfopers_jumlah_pns"] = None
+                            dsp_jabatan["sisfopers_jumlah_total"] = None
                             dsp_jabatan["sisfopers_parent_id"] = ""
                             dsp_jabatan["sisfopers_subsatuankerja_id"] = ""
-                            dsp_jabatan["compare_status"] = 1
+                            dsp_jabatan["sisfopers_parent_nomor"] = parent["dsp_nomor"]
+                            dsp_jabatan["compare_status"] = 0
+
+                else:
+                    dsp_jabatan["sisfopers_struktur_id"] = ""
+                    dsp_jabatan["sisfopers_jabatan_nama_panjang"] = ""
+                    dsp_jabatan["sisfopers_jumlah_perwira"] = None
+                    dsp_jabatan["sisfopers_jumlah_bintara"] = None
+                    dsp_jabatan["sisfopers_jumlah_tamtama"] = None
+                    dsp_jabatan["sisfopers_jumlah_pns"] = None
+                    dsp_jabatan["sisfopers_jumlah_total"] = None
+                    dsp_jabatan["sisfopers_parent_id"] = ""
+                    dsp_jabatan["sisfopers_subsatuankerja_id"] = ""
+                    dsp_jabatan["sisfopers_parent_nomor"] = parent["dsp_nomor"]
+                    dsp_jabatan["compare_status"] = 0
 
             dsp_jabatan_list.append(dsp_jabatan)
             counter = counter + 1
